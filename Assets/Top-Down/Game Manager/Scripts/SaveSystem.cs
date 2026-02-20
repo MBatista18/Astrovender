bba3e4c2784a327edf2f5512abf6210528e07a4b
ChaosDais <73835757+ChaosDais@users.noreply.github.com>
2026-02-20 17:40:14 -0500

    Basic Save System

---
+++ Assets/Top-Down/Game Manager/Scripts/SaveSystem.cs
@@ -0,0 +1,49 @@
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public static class SaveSystem
+{
+    private static readonly string path = Application.persistentDataPath + "/data.bin";
+
+    public static void SaveToFile(SaveData data)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static SaveData LoadFromFile()
+    {
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            SaveData data = formatter.Deserialize(stream) as SaveData;
+            stream.Close();
+
+            return data;
+        }
+        else
+        {
+            Debug.LogError("Save file not found in " + path);
+            return null;
+        }
+    }
+
+    public static void DeleteData()
+    {
+        if (File.Exists(path))
+        {
+            File.Delete(path);
+            Debug.Log("Save file deleted from " + path);
+        }
+        else
+        {
+            Debug.LogError("Save file not found in " + path);
+        }
+    }
+}
